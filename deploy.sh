bfb4325faf8024749b5e874861958b6db5dac0d8
Weijuer <ch_weijuer@163.com>
2019-02-20 14:02:10 +0800

    v1.0.1 发布脚本修改；

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:weijuer/blog.git gh-pages
+git push -f git@github.com:weijuer/blog.git master:gh-pages
 
 # 关闭窗口
-# cd -
+cd -
